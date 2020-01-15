a996e03094ea5a8101aab1462de6eef35190f192
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-01-15 08:51:45 -0500

    A1: Added random color generator to every pixel painted

--- Assignments/Assignment01/js/script.js
+++ Assignments/Assignment01/js/script.js
@@ -1,34 +1,63 @@
 "use strict";
 
+/********************************************************************
 
+Pixel painter
+Pippin Barr
 
+A small DOM-based program for "painting" on div-based pixels.
 
+*********************************************************************/
 
+// Constants
+const NUM_PIXELS = 1000;
+const PIXEL_REVERT_DELAY = 1000;
+const DEFAULT_COLOR = 'black';
+const PAINT_COLOR = 'white';
+
+// Set up our starting function for when the page loads
 window.onload = setup;
 
-
-
+// setup
+//
+// Adds DIVs to the page along with event listeners that will allow
+// then to change color on mouseover.
 function setup() {
-  console.log("PIPPIN IS AWESOME!!");
-
-  for (let i = 0; i < 1000; i++) {
+  // A loop that runs once per pixel we need
+  for (let i = 0; i < NUM_PIXELS; i++) {
+    // Create a DIV and store it in a variable
     let pixel = document.createElement('div');
-    pixel.setAttribute('class','pixel');
-    document.body.appendChild(pixel);
-
+    // Add the 'pixel' class to the new element
+    pixel.setAttribute('class', 'pixel');
+    // Add a mouseover handler to the new element
     pixel.addEventListener('mouseover', paint);
-}
-}
-
-function paint(e){
-let pixel = e.target;
-pixel.style.backgroundColor = "blue";
-setTimeout(resetPixel,1000, pixel);
-
+    // Add the element to the body of the page
+    document.body.appendChild(pixel);
+  }
 }
 
-function resetPixel(pixel){
+// paint
+//
+// Called by the mouseover event handler on each pixel. Changes
+// the pixel's color and sets a timer for it to revert
+function paint(e) {
+  // e.target contains the specific element moused over so let's
+  // save that into a variable for clarity.
+  let pixel = e.target;
+  // Change the background color of the element to white
+  pixel.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  // random color code found here =      https://www.sitepoint.com/get-url-parameters-with-javascript/
 
-pixel.style.backgroundColor = "black";
 
+  // Set a timeout to call the reset function after a delay
+  // When we pass additional parameters (like 'pixel' below) they
+  // are passed to the callback function (resetPixel)
+  setTimeout(resetPixel, PIXEL_REVERT_DELAY, pixel);
+}
+
+// resetPixel
+//
+// Takes the provided pixel element and sets its color back to default
+function resetPixel(pixel) {
+  pixel.style.backgroundColor = DEFAULT_COLOR;
 }
