9b234ed3416ac72280765cd4db0af38616b90b1d
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-01-28 17:09:23 -0500

    A2: updated comments and cleaned code

--- Assignments/Assignment02/js/script.js
+++ Assignments/Assignment02/js/script.js
@@ -5,6 +5,9 @@
 Raving Redactionist
 Pippin Barr
 
+Assignment02 edit
+Chrysoula Skalkogiannis 
+
 You are redacting a document, but it keeps becoming unredacted!
 Click the secret information to hide it, don't let all the
 secrets become revealed!
@@ -16,15 +19,20 @@ const REVEAL_POSSIBILITY = 0.1;
 // How often to update the spans (potentially revealing them)
 const UPDATE_FREQUENCY = 500;
 
-
+//our number of found secrets
 let secretsFound = 0;
 
+//our number of total secrets
 let secretsTotal;
 
 // A place to store the jQuery selection of all spans
 let $spans;
 
+// A place to store the selection of secrets
 let $secrets;
+
+
+
 // When the document is loaded we call the setup function
 $(document).ready(setup);
 
@@ -39,15 +47,18 @@ function setup() {
   // Set an interval of 500 milliseconds to update the state of the page
   setInterval(update, UPDATE_FREQUENCY);
 
+//total number of secrets is calculated by the secrets we have in our HTML
  secretsTotal = $(".secret").length;
  console.log(secretsTotal);
 
 
-
+//assign .secret as our variable
 $secrets = $('.secret');
-$secrets.on('mouseover', secretFound);
-//$secrets.off('mouseover', storeFoundSecret);
 
+//a secret is found when our mouse hovers over a secret in our text
+$secrets.on('mouseover', secretFound);
+
+//display the UI for secrets found at the botton of the page
 $('#scoreDisplay').text(secretsFound + " OF " + secretsTotal + " SECRETS FOUND");
 
 };
@@ -61,12 +72,15 @@ function spanClicked() {
   $(this).addClass('redacted');
 }
 
+//When a secret is clicked it Highlights yellow and 1 is added to our total secrets found
 function secretFound() {
 
   $(this).addClass('found');
   $(this).off('mouseover');
   secretsFound += 1;
   console.log(secretsFound);
+
+  //our UI updates based on the number of secrets found
   $('#scoreDisplay').text(secretsFound + " OF " + secretsTotal + " SECRETS FOUND");
 }
 
@@ -92,24 +106,3 @@ function updateSpan() {
     $(this).addClass('revealed');
   }
 }
-
-// A version using anonymous functions if you're interested:
-
-// $(document).ready(function () {
-//   $spans = $('span');
-//
-//   $spans.on('click',function () {
-//     $(this).removeClass('revealed');
-//     $(this).addClass('redacted');
-//   });
-//
-//   setInterval(function () {
-//     $spans.each(function () {
-//       let r = Math.random();
-//       if (r < REVEAL_POSSIBILITY) {
-//         $(this).removeClass('redacted');
-//         $(this).addClass('revealed');
-//       }
-//     });
-//   },UPDATE_FREQUENCY);
-// });
