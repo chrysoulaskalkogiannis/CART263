09b4122248e5b80c5a537bd13c8a9dc5a021499b
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-02-01 19:34:11 -0500

    P1: squares are removed when droped on black box

--- Projects/Project01/js/script.js
+++ Projects/Project01/js/script.js
@@ -1,14 +1,34 @@
 "use strict";
 
 
+let $base;
+let $square;
 
 $(document).ready(setup);
 
-function setup(){
+function setup() {
 
-$('.square').draggable();
+  $base = $(".base");
+
+  $base.droppable({
+
+    drop: onDrop
+  });
 
 
+  $square = $('.square');
+
+  $square.draggable({
+    revert: true
+  });
+
+}
+
+function onDrop(event, ui) {
+
+ui.draggable.remove();
+
+//  ({revert:false});
 
 
 }
