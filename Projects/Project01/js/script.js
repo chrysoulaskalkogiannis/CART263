ececb062ab86cfc565d3d6c6defc8500c86e2633
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-02-02 13:22:54 -0500

    P1: added an array of inspirational messages

--- Projects/Project01/js/script.js
+++ Projects/Project01/js/script.js
@@ -6,6 +6,10 @@ let $square;
 
 let totalSquares;
 
+let message = ['You can do it!', 'Almost there', 'Keep going', 'Just a little more', 'JUST DO IT!'];
+
+let random;
+
 $(document).ready(setup);
 
 function setup() {
@@ -27,6 +31,11 @@ function setup() {
 
   totalSquares = $(".square").length;
 
+
+  random = Math.floor ( Math.random() * message.length );
+  $('#messageDisplay').text(message[random])
+
+
 }
 
 function onDrop(event, ui) {
@@ -37,8 +46,9 @@ totalSquares -= 1;
 
 console.log(totalSquares);
 
-
 if (totalSquares == 0){
       location.reload(true);
+
+
 }
 }
