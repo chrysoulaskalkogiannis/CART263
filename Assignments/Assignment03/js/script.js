9a757876d7007504468eb74a38a1d232ae3b8fdd
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-02-18 22:27:23 -0500

    A3: 'I think it is X' working

--- Assignments/Assignment03/js/script.js
+++ Assignments/Assignment03/js/script.js
@@ -13,257 +13,273 @@ to match your project! Write JavaScript to do amazing things below!
 let score = 0;
 
 
-let animals =  [
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
-    ];
+let animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra"
+];
 
-  let correctAnimal;
-  let guess;
-  let answers = [];
-  const NUM_OPTIONS = 4;
+let correctAnimal;
+let guess;
+let answers = [];
+const NUM_OPTIONS = 4;
 
 
 // define the annyang command
-if(annyang) {
+if (annyang) {
 
-var commands = {
-  'say it again': function(){ sayBackwards(correctAnimal);}
-};
-
-//add our command sto annyang
-annyang.addCommands(commands);
-
-//start listening
-annyang.start();
-
-
-
-var commands2 = {
-  'I give up': function(){giveUp();}
-};
-
-//add our command sto annyang
-annyang.addCommands(commands2);
-
-//start listening
-annyang.start();
+  var commands = {
+    'say it again': function() {
+      sayBackwards(correctAnimal);
+    }
+  };
+  //add our command sto annyang
+  annyang.addCommands(commands);
+  //start listening
+  annyang.start();
 
 
+
+  var commands2 = {
+    'I give up': function() {
+      giveUp();
+    }
+  };
+  //add our command sto annyang
+  annyang.addCommands(commands2);
+  //start listening
+  annyang.start();
+
+
+
+  var commands3 = {
+    'I think it is *sayGuess': function(sayGuess) {
+      if (sayGuess === correctAnimal) {
+        $(".guess").remove();
+        setTimeout(newRound, 100);
+        score++;
+        scoreTrack();
+
+      }
+    }
+
+
+  };
+  //add our command sto annyang
+  annyang.addCommands(commands3);
+  //start listening
+  annyang.start();
 }
-
 
 
 $(document).ready(setup);
 
 function setup() {
-newRound();
-scoreTrack();
+  newRound();
+  scoreTrack();
 
 }
 
 
-function addButton(label){
+function addButton(label) {
 
 
-let $div = $('<div></div>');
-$div.addClass('guess');
-$div.text(label);
-$div.button();
-$div.appendTo('body');
-$div.on('click', handleGuess);
+  let $div = $('<div></div>');
+  $div.addClass('guess');
+  $div.text(label);
+  $div.button();
+  $div.appendTo('body');
+  $div.on('click', handleGuess);
 
 
 }
 
-function newRound(){
+function newRound() {
 
 
-$(".guess").remove();
-answers = [];
+  $(".guess").remove();
+  answers = [];
 
+  for (let i = 0; i < NUM_OPTIONS; i++) {
+    let ans = animals[Math.floor(Math.random() * animals.length)];
+    addButton(ans);
+    answers.push(ans);
 
+  }
 
-for (let i = 0; i < NUM_OPTIONS; i++) {
-  let ans = animals[Math.floor(Math.random()*animals.length)];
-addButton(ans);
-answers.push(ans);
-
-}
-
-correctAnimal = answers[Math.floor(Math.random()*answers.length)];
-sayBackwards(correctAnimal);
-
-}
-
-function handleGuess(){
-
-if ($(this).text()=== correctAnimal){
-
-score++;
-scoreTrack();
-setTimeout(newRound,500);
-}else{
-$('.guess').effect('shake');
-score = 0;
-scoreTrack();
+  correctAnimal = answers[Math.floor(Math.random() * answers.length)];
+  sayBackwards(correctAnimal);
 
 }
 
 
 
 
+function handleGuess() {
+
+  if ($(this).text() === correctAnimal) {
+
+    score++;
+    scoreTrack();
+    setTimeout(newRound, 500);
+  } else {
+    $('.guess').effect('shake');
+    score = 0;
+    scoreTrack();
+
+  }
 
 }
 
-function sayBackwards(text){
+function sayBackwards(text) {
 
-let backwardsText = text.split('').reverse().join('');
-let options = {
-pitch:Math.random(0,1),
-rate:Math.random(0,1)
-};
+  let backwardsText = text.split('').reverse().join('');
+  let options = {
+    pitch: Math.random(0, 1),
+    rate: Math.random(0, 1)
+  };
 
-responsiveVoice.speak(backwardsText,"UK English Female", options);
+  responsiveVoice.speak(backwardsText, "UK English Female", options);
 
 
 }
 
-function giveUp(){
+function giveUp() {
 
-newRound()
+  score = 0;
+  scoreTrack();
+  newRound()
 }
 
 
