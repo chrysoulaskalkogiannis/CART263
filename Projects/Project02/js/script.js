b446ab541b252687e691c39b137839d617818c33
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-02-25 09:07:37 -0500

    P2: speech working with name input variable

--- Projects/Project02/js/script.js
+++ Projects/Project02/js/script.js
@@ -9,9 +9,14 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+let playerName = prompt ("Please enter your name");
+
+
+
 
 $(document).ready(setup);
 
 function setup() {
-
+  console.log(playerName);
+responsiveVoice.speak("Hello"+ playerName + ",I want to play a game");
 }
