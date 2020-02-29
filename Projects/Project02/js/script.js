e733189e19f8d548999c6c3def1652c67889fb8d
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-02-28 19:57:15 -0500

    P2: added more story and scores for Profit, Trauma and Happiness

--- Projects/Project02/js/script.js
+++ Projects/Project02/js/script.js
@@ -11,7 +11,12 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 let playerName = prompt ("Please enter your name");
 
+let voice = 'UK English Male';
 
+let voiceParameters = {
+  pitch: 0.1,
+  rate: 0.9
+}
 
 
 $(document).ready(setup);
@@ -20,9 +25,7 @@ function setup() {
 
   console.log(playerName);
 
-responsiveVoice.speak("Hello "+ playerName + ". I want to play a game", "UK English Male",
-{pitch: 0.1,
-rate: 1});
+responsiveVoice.speak("Hello "+ playerName + ". I want to play a game. I want to make money, but I need your help.  I need you to approve of the content that will make the most profit for us.  Think you can manage better than the last person who tried to help me?  Alright then, let's begin",voice, voiceParameters)
 
 
-}
+};
