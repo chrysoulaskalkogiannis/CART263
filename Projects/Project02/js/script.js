a70d756a0eb36e433775378e79e594fd580a8920
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-03-03 09:19:51 -0500

    P2: added comaand for "I don't want to do this anymore"
    Added "I don't want to di this anymore" command as well as added a help to the main screen and can only say "Let's start" if player has clicked the CLICK TO START button

--- Projects/Project02/js/script.js
+++ Projects/Project02/js/script.js
@@ -19,8 +19,8 @@ let happinessScore = 0;
 
 
 
-let profitContent = ["Killing Peppa Pig", "Abuse", "Suicide Forest"];
-let happinessContent = ["Kittens", "Peppa Pig", ];
+let profitContent = ["Killing Peppa Pig", "Abuse", "Suicide Forest" ];
+let happinessContent = ["Kittens", "Peppa Pig", "Disney", "Baking"];
 
 
 
@@ -30,7 +30,7 @@ let buttons = [];
 const PROFIT_OPTIONS = 1;
 const HAPPINESS_OPTIONS = 1;
 
-let playerName =  "frank";     //prompt ("Please enter your name");
+let playerName = "frank"; //prompt ("Please enter your name");
 
 let profitButton;
 let happinessButton;
@@ -50,117 +50,113 @@ let voiceParameters = {
 
 
 
-
-
-
-
-
 var commands = {
-    "Let's start": function() {
-        startPlaying();
+  "Let's start": function() {
+    startPlaying();
 
-    }
-  };
-  annyang.addCommands(commands);
-  //start listening
-  annyang.start({ autoRestart: false, continuous: false });
+  }
+};
+
+//start listening
+annyang.start({
+  autoRestart: false,
+  continuous: false
+});
 
 
 
-/*
-  var commands2 = {
-      "I don't want to do this anymore": function() {
 
-      choices()  ;
-      }
-    };
-    annyang.addCommands(commands);
-    //start listening
-    annyang.start();
-*/
+var commands2 = {
+  "I don't want to do this anymore": function() {
+    doNotLeave();
+    letMeOut++
+  }
+};
+annyang.addCommands(commands2);
+//start listening
+annyang.start();
+
 
 
 $(document).ready(setup);
 
 function setup() {
 
-$('#mainScreen').hide();
+  $('.mainScreen').hide();
 
 
-$('.messageDisplay').text(profitScore);
-$('.messageDisplay').text(happinessScore);
+  $('.messageDisplay').text(profitScore);
+  $('.messageDisplay').text(happinessScore);
 
-$("#start").on("click", function(){
-$(this).hide(startPhrase());
-$('#mainScreen').show();
+  $("#start").on("click", function() {
+    $(this).hide(startPhrase());
+    $('.mainScreen').show();
 
-});
+    annyang.addCommands(commands);
+
+  });
 
 
 };
 
-function startPhrase(){
+function startPhrase() {
 
 
-  responsiveVoice.speak("Hello "+ playerName + ". I want to play a game. I want to make money, but I need your help.  I need you to approve of the content that will make the most profit for us.  Think you can manage better than the last person who tried to help me?  Alright then. Tell me when you're ready by saying. Let's start",voice, voiceParameters)
-
-}
-
-function startPlaying(){
-
-responsiveVoice.speak("Good. Now," + playerName + "which piece of content will give us more profit?", voice, voiceParameters)
-
-choices();
+  responsiveVoice.speak("Hello " + playerName + ". I want to play a game. I want to make money, but I need your help.  I need you to approve of the content that will make the most profit for us.  Think you can manage better than the last person who tried to help me?  Alright then. Tell me when you're ready by saying. Let's start", voice, voiceParameters)
 
 }
 
-function choices(){
+function startPlaying() {
 
+  responsiveVoice.speak("Good. Now," + playerName + "which piece of content will give us more profit?", voice, voiceParameters)
 
-
-buttons = [];
-
-for (let i = 0; i < PROFIT_OPTIONS; i++) {
-
-let answer = getRandomElement(profitContent);
-let $button = buttonShow(answer);
-   // Add this button to the buttons array
-   buttons.push($button);
-}
-
-for (let i = 0; i < HAPPINESS_OPTIONS; i++) {
-
-let answer = getRandomElement(happinessContent);
-let $button = buttonShow(answer);
-   // Add this button to the buttons array
-   buttons.push($button);
-}
-
-profitButton = getRandomElement(buttons);
-happinessButton = getRandomElement(buttons);
-
+  choices();
 
 }
 
+function choices() {
+
+  buttons = [];
+
+  for (let i = 0; i < PROFIT_OPTIONS; i++) {
+
+    let answer = getRandomElement(profitContent);
+    let $button = buttonShow(answer);
+    // Add this button to the buttons array
+    buttons.push($button);
+  }
+
+  for (let i = 0; i < HAPPINESS_OPTIONS; i++) {
+
+    let answer = getRandomElement(happinessContent);
+    let $button = buttonShow(answer);
+    // Add this button to the buttons array
+    buttons.push($button);
+  }
+
+  profitButton = getRandomElement(buttons);
+  happinessButton = getRandomElement(buttons);
 
 
-function buttonShow(label){
+}
 
 
+
+function buttonShow(label) {
 
   let $button = $('<div></div>');
-    // Give it the guess class
-    $button.addClass("guess");
-    // Set the text in the div to our label
-    $button.text(label);
-    // Turn the div into a button using jQuery UI's .button() method
-    $button.button();
-    // Listen for a click on the button which means the user has guessed
-    $button.on('click', handleButtonChoice);
-    // Finally, add the button to the page so we can see it
-    $('body').append($button);
-    // Return the button
-    return $button;
+  // Give it the guess class
+  $button.addClass("guess");
+  // Set the text in the div to our label
+  $button.text(label);
+  // Turn the div into a button using jQuery UI's .button() method
+  $button.button();
+  // Listen for a click on the button which means the user has guessed
+  $button.on('click', handleButtonChoice);
+  // Finally, add the button to the page so we can see it
+  $('body').append($button);
+  // Return the button
+  return $button;
 
 }
 
@@ -168,17 +164,19 @@ function buttonShow(label){
 
 
 
-function handleButtonChoice(){
+function handleButtonChoice() {
 
   if ($(this).text() === profitButton.text()) {
     // Remove all the buttons
     $(".guess").remove();
+    profitScore += 100;
+    happinessScore = happinessScore - 10;
     // Start a new round
     setTimeout(choices, 1000);
-  }
-  else  if ($(this).text() === happinessButton.text()) {
+  } else if ($(this).text() === happinessButton.text()) {
     // Remove all the buttons
     $('.guess').remove();
+    happinessScore += 100;
     // Start a new round
     setTimeout(choices, 1000);
   }
@@ -189,8 +187,24 @@ function handleButtonChoice(){
 
 
 
-  function getRandomElement(array) {
-    let element = array[Math.floor(Math.random() * array.length)];
-    return element;
+function getRandomElement(array) {
+  let element = array[Math.floor(Math.random() * array.length)];
+  return element;
+
+}
+
+function doNotLeave() {
+
+  if (letMeOut === 0) {
+
+    responsiveVoice.speak("But" + playerName + ". We are just getting started", voice, voiceParameters);
+  } else if (letMeOut === 1) {
+
+    responsiveVoice.speak(playerName + ". I want you to stay and make money.  I need you " + playerName, voice, voiceParameters);
+  } else if (letMeOut === 2) {
+    responsiveVoice.speak("I am hurt " + playerName + ".  Fine then. Go.  I will find someone else who is willing to help me.  I want you to leave now " + playerName, voice, voiceParameters);
+
+  }
+
 
 }
