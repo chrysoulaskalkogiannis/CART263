2a163cd0ee6fc48dcae3d735bb148ba48ef1a75c
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2020-04-12 20:59:20 -0400

    P3: Created name variable and added buttons

--- Projects/Project03/js/script.js
+++ Projects/Project03/js/script.js
@@ -9,3 +9,23 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+let playerName = prompt("Please enter your name");
+
+let robotVoice;
+
+
+
+
+
+$(document).ready(setup);
+
+function setup() {
+
+console.log(playerName);
+
+};
+
+
+function startPhrase() {
+  responsiveVoice.speak("Hello " + playerName)
+}
